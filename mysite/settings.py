a55ba6e19f890d7f9bf92b4fecb52670244b65c6
Andres Ribera <andresriberae@gmail.com>
2023-07-26 17:07:20 -0400

    config db

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,12 +74,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'uroot',
+        'PASSWORD': 'p8TFY2jQL4ssWPonXD9Oi',
+        'HOST': 'hcontainers-us-west-196.railway.app',
+        'PORT': '6181',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
