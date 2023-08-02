bf14a42a2b09e90d484a95499ff6ab9d8399367c
Andres Ribera <andresriberae@gmail.com>
2023-08-02 16:09:46 -0400

    customized commit messagge

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-%bxed!!yf*f@z9b5n*)hqva=m#kpr($a56o9dq17=gn&skryz1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -151,6 +152,9 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kcandres21@gmail.com'
 EMAIL_HOST_PASSWORD = 'wpuzmwfirywpizsq'
 EMAIL_USE_TLS = True
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIR = [
     BASE_DIR / "static"
