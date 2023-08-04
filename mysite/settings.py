6cf1d8b0fb0a3a8017bbf6691561265dc4c6540a
Andres Ribera <andresriberae@gmail.com>
2023-08-04 01:49:56 -0400

    staticfiles 003

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -139,6 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -153,8 +154,8 @@ EMAIL_HOST_USER = 'kcandres21@gmail.com'
 EMAIL_HOST_PASSWORD = 'wpuzmwfirywpizsq'
 EMAIL_USE_TLS = True
 
-STATICFILES_DIR = [
-    BASE_DIR / "static"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR , "static"),
 ]
 
 MEDIA_ROOT = BASE_DIR / "files"
